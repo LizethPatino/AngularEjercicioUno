cc96d06727c4dca1fbde18748ea105383b17ba74
LizetPatino <lizeth0330@outlook.com>
2019-02-01 21:27:46 -0500

    explicacion directiva estructural ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HeaderComponent } from './components/header/header.component';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
